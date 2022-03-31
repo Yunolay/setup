dfa1053bae96eb4d7ae1e67ae2c6e0b70a5c1dcb
Yunolay <87972392+Yunolay@users.noreply.github.com>
2022-03-31 13:31:05 +0900

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -43,6 +43,10 @@ cd msfvenom_maker
 cp msfvenom_maker ~/bin
 
 cd $HOME
+cd tools
+wget https://jitpack.io/com/github/frohoff/ysoserial/master-SNAPSHOT/ysoserial-master-SNAPSHOT.jar
+
+cd $HOME
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
