d5fde199abebf244adc3bab6547ce1f3bb23a96a
Yunolay <87972392+Yunolay@users.noreply.github.com>
2023-03-02 15:50:36 +0900

    Update kali_v2022_3.sh

--- kali_v2022_3.sh
+++ kali_v2022_3.sh
@@ -43,6 +43,21 @@ wget https://jitpack.io/com/github/frohoff/ysoserial/master-SNAPSHOT/ysoserial-m
 mv ysoserial-master-SNAPSHOT.jar ysoserial.jar
 
 cd $HOME
+cd tools
+sudo apt install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev libspdlog-dev python3-dev libboost-all-dev mingw-w64 nasm
+git clone https://github.com/HavocFramework/Havoc.git
+cd Havoc/Client
+make
+cd $HOME
+cd tools
+cd Havoc/Teamserver
+go mod download golang.org/x/sys  
+go mod download github.com/ugorji/go
+./Install.sh
+make
+./teamserver -h
+
+cd $HOME
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
